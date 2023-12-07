230bfec419c229ae1288ba785035493f888a86ed
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-12-07 15:31:39 +0900

    feat(lotto): 로또 번호를 검증하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    private static final int MINIMUM = 1;
+    private static final int MAXIMUM = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,27 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        ensureSizeOfNumbers(numbers);
+        ensureDifferentNumbers(numbers);
+        ensureRangeOfNumbers(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+    private void ensureSizeOfNumbers(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void ensureDifferentNumbers(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void ensureRangeOfNumbers(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < MINIMUM || number > MAXIMUM)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
