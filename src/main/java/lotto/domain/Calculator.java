bb6e3e1b23228699c0bb863e6f369c778accc24e
MSDN-SPECIAL\Administrator <jsw1767@gmail.com>
2023-12-15 13:51:03 +0900

    checkout 전 커밋

---
+++ src/main/java/lotto/domain/Calculator.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class Calculator {
+    public long sumWinningAmount(Map<Integer, Integer> winningTable) {
+        // winningAmout 상금
+        // value 당첨 개수
+        // key 순위(rank)
+        return 0;
+    }
+}
--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -6,4 +6,5 @@ public class OutputMessage {
     public static final String THIRD = "5개 일치 (1,500,000원) - %d개\n";
     public static final String SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n";
     public static final String FIRST = "6개 일치 (2,000,000,000원) - %d개\n";
+    public static final String RATE_OF_RETRUN = "총 수익률은 %d%s입니다.";
 }
